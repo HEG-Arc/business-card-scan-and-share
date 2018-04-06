515b5051a579f8bef5a12ce70e76671e089b4577
Boris Fritscher <boris@fritscher.ch>
2018-04-06 13:59:57 +0200

    upgrade firebase functions to sdk 1.0.0

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -3,44 +3,47 @@ const gcs = require('@google-cloud/storage')();
 const vision = require('@google-cloud/vision');
 const language = require("@google-cloud/language");
 const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 const db = admin.firestore();
 
 const DB_ROOT = "business-card-app";
 
 exports.businessCardExtractText = functions.storage
   .object()
-  .onChange(event => {
-    const object = event.data;
+  .onFinalize((object, context) => {
     const file = gcs.bucket(object.bucket).file(object.name);
-    const filePath = event.data.name;
+    const filePath = object.name;
     const filePathSplit = filePath.split("/");
     const fileName = filePathSplit.pop();
 
     // Exit if this is triggered on a file that is not an image.
-    if (!event.data.contentType.startsWith("image/")) {
-      return console.log("This is not an image.");
+    if (!object.contentType.startsWith("image/")) {
+      console.log("This is not an image.");
+      return "";
     }
 
     // Exit if the image is already a thumbnail.
     if (fileName.includes("_match")) {
-      return console.log("ignore _match files");
-    }
-
-    // Exit if this is a move or deletion event.
-    if (event.data.resourceState === "not_exists") {
-      return console.log("This is a deletion event.");
+      console.log("ignore _match files");
+      return "";
     }
 
     if (filePath.indexOf(DB_ROOT) === -1) {
-      return console.log(`not a ${DB_ROOT} app`);
+      console.log(`not a ${DB_ROOT} app`);
+      return "";
     }
     const vClient = new vision.ImageAnnotatorClient();
     return vClient
       .textDetection(`gs://${object.bucket}/${object.name}`)
       .then(results => {
+        if (!results || results.length === 0) {
+          return;
+        }
         const detections = results[0].textAnnotations;
         const lClient = new language.LanguageServiceClient();
+        if (!detections || detections.length === 0) {
+          return;
+        }
         const text = detections[0].description
         const document = {
           content: text,
@@ -51,6 +54,9 @@ exports.businessCardExtractText = functions.storage
             document: document
           })
           .then(results => {
+            if (!results || results.length === 0) {
+              return;
+            }
             const entities = results[0].entities;
             const dbRef = db.collection(`${DB_ROOT}/data/cards`).doc(fileName.slice(0, fileName.length - 4));
             return dbRef.update({
@@ -68,3 +74,4 @@ exports.businessCardExtractText = functions.storage
         return console.error("ERROR:", err);
       });
   });
+
